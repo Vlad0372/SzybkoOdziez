41704984223985fc01eb9eb477d270258c690b45
toniejaxD <karolkoodynski@gmail.com>
2023-03-22 20:49:10 +0100

    Koszy, zatwierdzenie, informacje..., produky inf..

---
+++ SzybkoOdziez/SzybkoOdziez/Produkt_informacje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SzybkoOdziez
+{
+    public class Produkt_informacje
+    {
+        public int ProduktId { get; set; }
+        public string NazwaProduktu { get; set; }
+
+        public string Cena { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
