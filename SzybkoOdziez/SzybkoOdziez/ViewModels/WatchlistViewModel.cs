8253315c894b831c0d110cada43800abbab7d44f
michalgoralski <michal.goralski@student.po.edu.pl>
2023-03-28 09:29:22 +0200

    Dokladniejszy podglad przedmiotu z listy obserwowanych przedmiotów. Wyswietla strone ItemDescriptionPage.

--- SzybkoOdziez/SzybkoOdziez/ViewModels/WatchlistViewModel.cs
+++ SzybkoOdziez/SzybkoOdziez/ViewModels/WatchlistViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using SzybkoOdziez.Models;
 using SzybkoOdziez.Views;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -8,9 +11,18 @@ namespace SzybkoOdziez.ViewModels
 {
     public class WatchlistViewModel : BaseViewModel
     {
+        public ObservableCollection<ProductInfo> Products { get; set; }
+
         public WatchlistViewModel()
         {
-            Title = "Lista obserwowanych /nuwu/nprzedmiotów";
+            List<string> imgsNameList = DependencyService.Get<IImgArrayGetterService>().GetImgArrayStreamAsync();
+
+            Products = new ObservableCollection<ProductInfo>
+            {
+            new ProductInfo { Id = 1, Name = "HAHAHA", Description = "OKEJ, CZY TO DZIALA?!?!", Price = "5.00 zl", Url = "@drawable/" + imgsNameList[1] + ".jpg" }
+            };
+
+            
         }
     }
 }
--- SzybkoOdziez/SzybkoOdziez/Views/WatchlistPage.xaml.cs
+++ SzybkoOdziez/SzybkoOdziez/Views/WatchlistPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SzybkoOdziez.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,10 +12,28 @@ namespace SzybkoOdziez.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WatchlistPage : ContentPage
-	{
-		public WatchlistPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    {
+        public WatchlistPage()
+        {
+            InitializeComponent();
+
+
+            BindingContext = new ViewModels.WatchlistViewModel();
+            
+        }
+
+
+        private void OnWishlistProductStackLayoutTapped(object sender, EventArgs e)
+        {
+            ChangePageToItemDescription();
+        }
+
+
+        private void ChangePageToItemDescription()
+        {
+            Navigation.PushAsync(new ItemDescriptionPage());
+        }
+    }
+
+
 }
