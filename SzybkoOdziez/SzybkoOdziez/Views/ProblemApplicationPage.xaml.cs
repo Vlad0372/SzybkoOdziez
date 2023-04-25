56c1ecb3eaa044e4b73623ff05ed39dff332e441
toniejaxD <karolkoodynski@gmail.com>
2023-04-25 10:12:12 +0200

    Add in hamburger "Zgłoszenie problemu aplikacji" in "AppShell.xaml", add layout in "ProblemApplicationPage.xaml" and show message about send problem application in "GetMessageAboutProblemApplicationPage.xaml"

---
+++ SzybkoOdziez/SzybkoOdziez/Views/ProblemApplicationPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SzybkoOdziez.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProblemApplicationPage : ContentPage
+    {
+        public ProblemApplicationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void send_message_problem_application_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GetMessageAboutProblemApplicationPage());
+            Navigation.RemovePage(this);
+        }
+    }
+}
