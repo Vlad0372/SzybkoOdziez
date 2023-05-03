0d8bebe0392e4708f7efb9209c92de2bfde84898
michalgoralski <michal.goralski@student.po.edu.pl>
2023-05-03 11:13:42 +0200

    komentarze

---
+++ SzybkoOdziez/SzybkoOdziez/Services/IPhotoPickerService.cs
@@ -0,0 +1,13 @@
+﻿using Android.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzybkoOdziez.Services
+{
+    public interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
--- SzybkoOdziez/SzybkoOdziez/Views/ProductCommentsPage.xaml.cs
+++ SzybkoOdziez/SzybkoOdziez/Views/ProductCommentsPage.xaml.cs
@@ -12,6 +12,9 @@ using Xamarin.Forms.Xaml;
 using Android.Provider;
 using System.Windows;
 using Acr.UserDialogs;
+using Xamarin.Essentials;
+using Android.Graphics;
+using System.IO;
 
 namespace SzybkoOdziez.Views
 {
@@ -69,5 +72,100 @@ namespace SzybkoOdziez.Views
             }
             return isUnique;
         }
+
+        //private async void PickImage_Clicked(object sender, EventArgs e)
+        //{
+        //    string filePath = "";
+        //    try
+        //    {
+        //        var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+        //        {
+        //            Title = "Pick an image"
+        //        });
+
+        //        if (result != null)
+        //        {
+        //            var stream = await result.OpenReadAsync();
+        //            ImageComment.Source = ImageSource.FromStream(() => stream);
+        //            Console.Write(result);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        await DisplayAlert("Nie dziala", "ZEPSUTE", "ok...");
+        //    }
+        //}
+
+
+        //private async void Button_Clicked(object sender, EventArgs e)
+        //{
+
+        //    await TakePhotoAsync();
+        //}
+
+        //string PhotoPath;
+        //async Task TakePhotoAsync()
+        //{
+        //    try
+        //    {
+        //        var photo = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+        //        {
+        //            Title = "Please pick a photo to use as an Avatar..."
+        //        });
+        //        await LoadPhotoAsync(photo);
+        //        Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+
+        //        await DisplayAlert("in", PhotoPath, "OK");
+        //    }
+        //    catch (FeatureNotSupportedException fnsEx)
+        //    {
+        //        // Feature is not supported on the device  
+        //    }
+        //    catch (PermissionException pEx)
+        //    {
+        //        // Permissions not granted  
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+        //    }
+        //}
+
+        //async Task LoadPhotoAsync(FileResult photo)
+        //{
+        //    // canceled  
+        //    if (photo == null)
+        //    {
+        //        PhotoPath = null;
+        //        return;
+        //    }
+        //    // save the file into local storage  
+        //    var newFile = System.IO.Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+
+
+        //    using (var stream = await photo.OpenReadAsync())
+        //    using (var newStream = File.OpenWrite(newFile))
+        //        await stream.CopyToAsync(newStream);
+
+        //    PhotoPath = newFile;
+        //}
+
+
+        //private async void Button_Clicked2(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        var result = await MediaPicker.PickPhotoAsync();
+        //        if (result != null)
+        //        {
+        //            ImageComment.Source = result.FullPath;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Handle any exceptions that occur
+        //    }
+        //} 
+
     }
 }
