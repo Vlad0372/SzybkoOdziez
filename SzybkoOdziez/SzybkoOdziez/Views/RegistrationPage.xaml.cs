dc791e9c496f5904b70c10fa6b689c54f566896d
toniejaxD <karolkoodynski@gmail.com>
2023-05-05 16:38:53 +0200

    add address from SIMON in data base in REGISTRATIONPAGE

--- SzybkoOdziez/SzybkoOdziez/Views/RegistrationPage.xaml.cs
+++ SzybkoOdziez/SzybkoOdziez/Views/RegistrationPage.xaml.cs
@@ -9,6 +9,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using static Android.Graphics.ColorSpace;
 using static Java.Util.Jar.Attributes;
+using Oracle.ManagedDataAccess.Client;
 
 namespace SzybkoOdziez.Views
 {
@@ -23,7 +24,12 @@ namespace SzybkoOdziez.Views
 
         private async void create_account_Button_Clicked(object sender, EventArgs e)
         {
-
+            string ConnectionString = "Data Source=(DESCRIPTION=" +
+            "(ADDRESS=(PROTOCOL=TCP)(HOST=217.173.198.135)(PORT=1521))" +
+            "(CONNECT_DATA=(SERVICE_NAME=tpdb)));" + "" +
+            "User Id=s100824;Password=Sddb2023;";
+            OracleConnection connection = new OracleConnection(ConnectionString);
+            connection.Open();
 
 
             if (string.IsNullOrEmpty(area_Name.Text)|| string.IsNullOrEmpty(area_Surname.Text) || string.IsNullOrEmpty(area_Name_user.Text) || string.IsNullOrEmpty(area_PASSWORD.Text) || string.IsNullOrEmpty(area_E_MAIL.Text))
