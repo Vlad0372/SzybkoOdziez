51a33b2e83fa9391239ed2a35c02cfa546a26d0f
toniejaxD <karolkoodynski@gmail.com>
2023-05-15 19:04:06 +0200

    xDDDDDD UserProfilePage

--- SzybkoOdziez/SzybkoOdziez/Views/UserProfilePage.xaml.cs
+++ SzybkoOdziez/SzybkoOdziez/Views/UserProfilePage.xaml.cs
@@ -17,66 +17,99 @@ namespace SzybkoOdziez.Views
         public UserProfilePage()
         {
             InitializeComponent();
-
-
-
             // OGARNĄĆ ŻEBY INNY UŻYTKOWNIK ZOBACZYŁ SWOJE DANE!!!!!
-            // int userId = GetLoggedInUserId();
+            string Namee = "";
+            string LastNamee = "";
+            string Maill = "";
+            string Nicknamee = "";
+            string Passwordd = "";
+            int userId = GetLoggedInUserID(Nicknamee, Passwordd);
 
 
-            int userId = 1;
-
+            
             string ConnectionString = "Data Source=(DESCRIPTION=" +
                "(ADDRESS=(PROTOCOL=TCP)(HOST=217.173.198.135)(PORT=1521))" +
                "(CONNECT_DATA=(SERVICE_NAME=tpdb)));" + "" +
                "User Id=s100824;Password=Sddb2023;";
             OracleConnection connection = new OracleConnection(ConnectionString);
             connection.Open();
-            
+
             using (OracleConnection conn = new OracleConnection(ConnectionString))
             {
                 conn.Open();
 
                 var query = "SELECT name, last_name, mail, nickname from \"user\" where user_id = :id";
 
-                
+
                 var param = new OracleParameter(":id", OracleDbType.Int32);
                 param.Value = userId;
                 using (var cmd = new OracleCommand(query, conn))
-    {
+                {
                     cmd.Parameters.Add(param);
 
                     using (var reader = cmd.ExecuteReader())
                     {
+                      
+                        
                         if (reader.Read())
                         {
-                           
 
-                            string Namee = reader.GetString(0);
-                            string LastNamee = reader.GetString(1);
-                            string Maill = reader.GetString(2);
-                            string Nicknamee = reader.GetString(3);
-                           
+
+                             Namee = reader.GetString(0);
+                             LastNamee = reader.GetString(1);
+                             Maill = reader.GetString(2);
+                             Nicknamee = reader.GetString(3);
+
 
                             // przypisanie imienia i nazwiska użytkownika do kontrolki Label
-                            labelName.Text = Namee;
-                            labelLastName.Text = LastNamee;
-                            labelMail.Text = Maill;
-                            labelNickname.Text = Nicknamee;
                             
+
                         }
+                        reader.Close();
+                        labelName.Text = Namee;
+                        labelLastName.Text = LastNamee;
+                        labelMail.Text = Maill;
+                        labelNickname.Text = Nicknamee;
                     }
                 }
             }
+
+
+
         }
 
-    
+        public int GetLoggedInUserID(string nicknamee, string passwordd)
+        {
+            string ConnectionString = "Data Source=(DESCRIPTION=" +
+               "(ADDRESS=(PROTOCOL=TCP)(HOST=217.173.198.135)(PORT=1521))" +
+               "(CONNECT_DATA=(SERVICE_NAME=tpdb)));" + "" +
+               "User Id=s100824;Password=Sddb2023;";
+            int userId = -1;
+            using (OracleConnection connection = new OracleConnection(ConnectionString))
+            {
+                connection.Open();
+                var query = "SELECT user_id from \"user\" where nickname = :nickname AND password = :password";
+                OracleCommand command = new OracleCommand(query, connection);
+                command.Parameters.Add("nickname", nicknamee);
+                command.Parameters.Add("password", passwordd);
+                OracleDataReader reader = command.ExecuteReader();
 
-        
-            
+                if (reader.Read())
+                {
+                    userId = reader.GetInt32(0);
+                }
 
-        
+                reader.Close();
 
-        
+                return userId;
+            }
+        }
+
+
+
+
+
+
+
     }
 }
