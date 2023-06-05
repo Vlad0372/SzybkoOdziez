92c36b2939e09eebb8fa8772bcd7e15b31c7b24d
michalgoralski <michal.goralski@student.po.edu.pl>
2023-06-05 22:11:42 +0200

    Implemented carousel for items (ItemCarouselPage and ItemCarouselViewModel). Replaced going to item details from observed, instead it now navigates to the carousel.

---
+++ SzybkoOdziez/SzybkoOdziez/Views/ItemCarouselPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SzybkoOdziez.Models;
+using SzybkoOdziez.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SzybkoOdziez.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemCarouselPage : ContentPage
+    {
+        private ItemCarouselViewModel _viewModel;
+        public ItemCarouselPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new ItemCarouselViewModel();
+        }
+
+        public ItemCarouselPage(Product product)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new ItemCarouselViewModel(product);
+        }
+    }
+}
